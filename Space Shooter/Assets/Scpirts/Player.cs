9ccf8b1dee7238fe21f197f2601186d56856a527
Mary <ysters14@gmail.com>
2020-07-20 11:50:03 +0300

    Go through first challenge: add vertical direction

---
+++ Space Shooter/Assets/Scpirts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * Time.deltaTime * _speed * horizontalInput);
+        transform.Translate(Vector3.up * Time.deltaTime * _speed * verticalInput);
+    }
+}
