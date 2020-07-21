92a90dd970e17c091a544f07163824284b251084
Mary <ysters14@gmail.com>
2020-07-21 06:57:50 +0300

    Add laser, prefabs, solve challenge: write movement for laser

--- Space Shooter/Assets/Scpirts/Player.cs
+++ Space Shooter/Assets/Scpirts/Player.cs
@@ -7,11 +7,11 @@ public class Player : MonoBehaviour
 {
     [SerializeField]
     private float _speed = 3.5f;
-   
+    [SerializeField]
+    private GameObject _laserPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        //take the current position = new position (0, 0, 0)
         transform.position = new Vector3(0, 0, 0);
     }
 
@@ -19,6 +19,11 @@ public class Player : MonoBehaviour
     void Update()
     {
         Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab, transform.position, Quaternion.identity);
+        }
     }
 
     void Move()
