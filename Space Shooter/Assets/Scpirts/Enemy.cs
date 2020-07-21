f0adfd78e5d60c8d4a3d09417ac3db89aeff2687
Mary <ysters14@gmail.com>
2020-07-21 09:07:07 +0300

    Add enemy, solve challenge: add random spawn to enemy

---
+++ Space Shooter/Assets/Scpirts/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] 
+    private float _speed = 4.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (transform.position.y >= -5.6f)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * _speed);
+        }
+        else
+        {
+            transform.position = new Vector3(Random.Range(-8.0f, 8.0f), 7, 0);
+        }
+    }
+}
