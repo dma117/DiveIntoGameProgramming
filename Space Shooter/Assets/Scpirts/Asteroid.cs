c53533a644ff4f418d6c2b24bcb3061dcfb0eeff
Mary <ysters14@gmail.com>
2020-07-24 07:05:35 +0300

    Add explosion for asteroid with animation

--- Space Shooter/Assets/Scpirts/Asteroid.cs
+++ Space Shooter/Assets/Scpirts/Asteroid.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,10 +7,22 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField] 
     private float _speed;
+    [SerializeField]
+    private GameObject _explosionPrefab;
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(new Vector3(0, 0, _speed));
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
     }
 }
