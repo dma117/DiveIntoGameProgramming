1473c29dc39f6541b2c986a7fcffd72aea9294af
Mary <ysters14@gmail.com>
2020-07-28 06:12:42 +0300

    Create a new game 2.5D platformer, add characters and movement of the player

---
+++ 2.5D Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    
+    private CharacterController _characterController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalAxis = Input.GetAxis("Horizontal");
+        _characterController.Move(new Vector3(horizontalAxis, 0, 0) * Time.deltaTime * _speed);
+    }
+}
